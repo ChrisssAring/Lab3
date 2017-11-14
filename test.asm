c1c9b05e50d0ea62a8bf5e4d4856bb59faa8f68a
Christopher Aring <ChrisssAring@gmail.com>
2017-11-13 23:26:51 -0500

    updated commenting

--- test.asm
+++ test.asm
@@ -1,9 +1,17 @@
+# If a0 equals either $a1 or $a2 set $s0 to 1. Else, 0.
+
+# Set registers
 li $a0, 1
 li $a1, 4
 li $a2, 1
+
+# Call function
 jal test
+
+# Store answer in $s0
 move $s0, $v0
 
+# Call again
 li $a0, 1
 li $a1, 5
 li $a2, 5
@@ -13,6 +21,7 @@ move $s1, $v0
 li $v0, 10
 syscall
 
+# Test function
 test:
 beq  $a0, $a1, If
 bne  $a0, $a2, Else
