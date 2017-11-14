32e6848a3040b4bb0d7a267c316dc4dd65dd7ca4
Christopher Aring <ChrisssAring@gmail.com>
2017-11-14 00:15:19 -0500

    updated

--- test.asm
+++ test.asm
@@ -1,35 +1,29 @@
-# If a0 equals either $a1 or $a2 set $s0 to 1. Else, 0.
+# Function adds sum of N integers. 1 + 2 + ... +
 
-# Set registers
-li $a0, 1
-li $a1, 4
-li $a2, 1
+main:
+	li $a0, 0 # Counter
+	li $v0, 0 # Sum
+	li $a1, 4 # N
+	jal loop
+	
+	# Save answer to $s0
+	addi $s0, $v0, 0
+	
+	li $a0, 0
+	li $v0, 0
+	li $a1, 3
+	jal loop
+	
+	# Save answer to $s0
+	addi $s1, $v0, 0
+	
+	li $v0, 10
+	syscall 
 
-# Call function
-jal test
+loop:	addi $a0, $a0, 1 # Counter ++
+	add $v0, $v0, $a0 # Add sums
+	bne $a1, $a0, loop # End of loop
+	j end
 
-# Store answer in $s0
-move $s0, $v0
-
-# Call again
-li $a0, 1
-li $a1, 5
-li $a2, 5
-jal test2
-move $s1, $v0
-
-li $v0, 10
-syscall
-
-# Test function
-test:
-beq  $a0, $a1, If
-bne  $a0, $a2, Else
-
-If:
-	addi $v0, $v0, 1
-      	jr $ra
-      
-Else:
-	addi $v0, $v0, 0
+end:	
 	jr $ra
