d7a269fa5ff48ccb853168e72b37346a3fc294d4
Christopher Aring <ChrisssAring@gmail.com>
2017-11-16 20:30:43 -0500

    added pc parts

--- cpu.v
+++ cpu.v
@@ -1,10 +1,13 @@
+`include "instructionMemory.v"
 module cpu();
 
 wire clk;
 
-
 // Program Counter
-
+reg [31:0] programCounter;
+initial programCounter = 0;
+wire [31:0] programCounterPlus4;
+assign programCounterPlus4 = programCounter + 4;
 
 // Instruction Memory 
 
@@ -22,6 +25,10 @@ wire [2:0] aluCommand;
 wire [31:0] aluA;
 wire [31:0] aluB;
 
+//Instruction Memory
+wire [31:0] instructionData;
+instructionMemory instructionMemory(clk, 1'b0, {2'b0, programCounter [31:2]}, 32'b0, instructionData);
+
 ALU ALUcontrolLUT(.cout(aluCout), .flag(aluFlag), .finalsignal(aluResult), .ALUCommand(aluCommand), .a(aluA), .b(aluB));
 
 
@@ -32,5 +39,7 @@ wire [31:0] dataMemoryAddress;
 
 dataMemory dataMemory(.clk(clk), .dataOut(dataMemoryOut), .writeEnable(), .dataIn(dataMemoryIn), .address(dataMemoryAddress));
 
+// Program Counter Cont.
+// need to assign once we have more info.
 
 endmodule
