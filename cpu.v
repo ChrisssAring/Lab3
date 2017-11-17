ad1d6031d74949e6e37542fa5f570dcc0003c9a1
Christopher Aring <ChrisssAring@gmail.com>
2017-11-16 21:10:02 -0500

    update cpu and mem

--- cpu.v
+++ cpu.v
@@ -1,22 +1,32 @@
 `include "instructionMemory.v"
+
+
 module cpu();
 
 wire clk;
 
-// Program Counter
-reg [31:0] programCounter;
-initial programCounter = 0;
-wire [31:0] programCounterPlus4;
-assign programCounterPlus4 = programCounter + 4;
+//Program Counter
+wire [1:0] pcSelect;
+reg [31:0] pc = 0;
+wire [31:0] pcPlus4;
+assign pcPlus4 = pc + 4;
+wire [31:0] pcNext;
+wire [31:0] instruction;
 
-// Instruction Memory 
-
+//Instruction Memory
+instructionMemory instructionMem(instruction);
 
 //Instruction Decoder
+wire [5:0] opCode;
+wire [4:0] rs, rt, rd, shamt;
+wire [5:0] func;
+wire [15:0] imm;
+wire [25:0] jadress;
+wire [31:0] jumpAddress;
+wire [31:0] branchAddress;
 
 
 // Register
-
 
 // ALU
 wire aluCout;
@@ -24,10 +34,6 @@ wire aluFlag;
 wire [2:0] aluCommand;
 wire [31:0] aluA;
 wire [31:0] aluB;
-
-//Instruction Memory
-wire [31:0] instructionData;
-instructionMemory instructionMemory(clk, 1'b0, {2'b0, programCounter [31:2]}, 32'b0, instructionData);
 
 ALU ALUcontrolLUT(.cout(aluCout), .flag(aluFlag), .finalsignal(aluResult), .ALUCommand(aluCommand), .a(aluA), .b(aluB));
 
@@ -40,6 +46,7 @@ wire [31:0] dataMemoryAddress;
 dataMemory dataMemory(.clk(clk), .dataOut(dataMemoryOut), .writeEnable(), .dataIn(dataMemoryIn), .address(dataMemoryAddress));
 
 // Program Counter Cont.
-// need to assign once we have more info.
-
+always @(posedge clk) begin
+	pc <= pcNext;
+end
 endmodule
--- instructionMemory.v
+++ instructionMemory.v
@@ -1,20 +1,11 @@
 module instructionMemory
 (
-  input clk, regWE,
-  input[9:0] Addr,
-  input[31:0] DataIn,
-  output[31:0]  DataOut
+  input[31:0] address,
+  output [31:0] dataOut
 );
-  
-  reg [31:0] mem[1023:0];  
-  
-  always @(posedge clk) begin
-    if (regWE) begin
-      mem[Addr] <= DataIn;
-    end
-  end
-  
-  initial $readmemh("data.dat", mem);
-    
-  assign DataOut = mem[Addr];
+
+  reg [31:0] mem[0:1023];
+  initial $readmemh("memory.dat", mem);
+
+  assign dataOut = mem[address];
 endmodule
