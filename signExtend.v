2ebde22a0e2b94d980ac1e28fae7f7c9919a6785
Christopher Aring <ChrisssAring@gmail.com>
2017-11-16 23:30:46 -0500

    fixed

--- signExtend.v
+++ signExtend.v
@@ -1,11 +1,11 @@
-module signExtend(imm, signExtend);
+module signExtend(imm, signExt);
 	input[15:0] imm;
-	output[31:0] signExtend;
+	output[31:0] signExt;
 
 	wire [15:0] imm;
-	wire [31:0] signExtend;
+	wire [31:0] signExt;
 
-	assign signExtend[15:0] = imm[15:0];
-	assign signExtend[31:16] = {16 {imm[15]}};
+	assign signExt[15:0] = imm[15:0];
+	assign signExt:[31:16] = {16 {imm[15]}};
 
 endmodule
